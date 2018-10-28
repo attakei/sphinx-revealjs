70e2bd5221f7243d0b4b5bb5ba2effc4d90c0ef8
Kazuya Takei <attakei@gmail.com>
2018-10-28 11:54:59 +0900

    Debugging output

--- sphinx_revealjs/__init__.py
+++ sphinx_revealjs/__init__.py
@@ -1,16 +1,24 @@
 """
 """
+from pathlib import Path
+
 from sphinx.application import Sphinx
 
 from .builders import RevealjsHTMLBuilder
 
-
 __version__ = '0.1.0'
+
+
+def get_theme_path(name: str) -> Path:
+    here = Path(__file__).parent
+    return here / 'themes' / name
 
 
 def setup(app: Sphinx):
     app.add_config_value('revealjs_version', '3.7.0', 'html')
     app.add_builder(RevealjsHTMLBuilder)
+    app.add_html_theme(
+        'revealjs_debugging', str(get_theme_path('debugging')))
     return {
         'version': __version__,
         'env_version': 1,
