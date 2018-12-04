2ba3d681c0e1afe37cd20716fbf5f537efcd59e5
Kazuya Takei <attakei@gmail.com>
2018-12-04 10:06:55 +0900

    Add minimum test document case

---
+++ tests/test_sections.py
@@ -0,0 +1,16 @@
+import unittest
+from pathlib import Path
+
+from sphinx_testing import with_app
+
+PROJECT_ROOT = Path(__file__).parents[1]
+SPHINX_TESTAPP_CONF = {
+    'buildername': 'revealjs',
+    'srcdir': str(PROJECT_ROOT / 'tests' / 'testdocs'),
+    'copy_srcdir_to_tmpdir': True,
+}
+
+class DemoMakeTesting(unittest.TestCase):
+    @with_app(**SPHINX_TESTAPP_CONF)
+    def test_build_ok(self, app, status, warning):
+        app.build()
