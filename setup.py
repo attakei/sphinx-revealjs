a37079e0b010ab84ad707e4f3af745ad5fa408b2
Kazuya Takei <attakei@gmail.com>
2019-07-03 08:11:07 +0900

    Version is only specifed in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 """Setup module
 """
+import re
+from pathlib import Path
 from setuptools import setup
 
 
-setup()
+here = Path(__file__).parent
+
+
+def fetch_version_string(target: Path) -> str:
+    line_re = re.compile(r"__version__ = '(.*?)'", re.S)
+    return line_re.search(target.open().read()).group(1)
+
+
+setup(
+    version=fetch_version_string(here / 'sphinx_revealjs' / '__init__.py')
+)
