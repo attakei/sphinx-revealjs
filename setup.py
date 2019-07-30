d4e7a53f9301e2ea619ce5457435474cf3e34f31
Kazuya Takei <attakei@gmail.com>
2019-07-30 14:55:27 +0900

    Merge flake8 catchable tests

--- setup.py
+++ setup.py
@@ -16,6 +16,12 @@ install_requires = [
 ]
 
 extra_requires = {
+    'linting': [
+        'flake8',
+        'flake8-docstrings',
+        'flake8-isort',
+        'doc8',
+    ],
     'testing': [
         'nose',
         'sphinx-testing',
