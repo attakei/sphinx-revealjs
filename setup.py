b475dc498d369005165036673266f9d5040efc29
Kazuya Takei <attakei@gmail.com>
2019-07-30 20:08:03 +0900

    Migration simple metada into setup.cfg

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ from setuptools import find_packages, setup
 
 here = Path(__file__).parent
 
-with (here / 'README.rst').open(encoding='utf-8') as f:
-    long_description = f.read()
-
 install_requires = [
     'docutils',
     'Sphinx'
@@ -36,22 +33,7 @@ def fetch_version_string(target: Path) -> str:
 
 
 setup(
-    name='sphinx-revealjs',
     version=fetch_version_string(here / 'sphinx_revealjs' / '__init__.py'),
-    description='Sphinx extension with theme to generate Reveal.js presentation',  # noqa
-    long_description=long_description,
-    url='https://github.com/attakei/sphinx-revealjs',
-    author='attakei',
-    author_email='attakei@gmail.com',
-    classifiers=[  # Optional
-        'Development Status :: 3 - Alpha',
-        'Framework :: Sphinx',
-        'Framework :: Sphinx :: Extension',
-        'Framework :: Sphinx :: Theme',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Multimedia :: Graphics :: Presentation',
-    ],
     packages=find_packages(exclude=['docs', 'demo', 'tools']),
     install_requires=install_requires,
     extras_require=extra_requires,
