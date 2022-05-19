4970d12d0d05a69da2c8cc8612ad39780d1716a2
Kazuya Takei <myself@attakei.net>
2022-05-20 00:20:35 +0900

    ci: Move directive test cases

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -15,9 +15,3 @@ def test_ignore_comment_at_default_config(app, status, warning):  # noqa
 def test_comment_as_notes(app, status, warning):  # noqa
     soup = soup_html(app, "with_commented_notes.html")
     assert len(soup.find_all("aside")) == 1
-
-
-@pytest.mark.sphinx("revealjs", testroot="misc")
-def test_content_escape(app, status, warning):  # noqa
-    soup = soup_html(app, "with_notes_include_html.html")
-    assert len(soup.find_all("aside")) == 1
---
+++ tests/test_directives/test_slide.py
@@ -0,0 +1,27 @@
+"""Test cases for ``revealjs_slide`` directive."""
+import pytest
+from sphinx.testing.util import SphinxTestApp
+from testutils import soup_html
+
+
+@pytest.mark.sphinx("revealjs", testroot="default")
+def test_override_theme(app: SphinxTestApp, status, warning):  # noqa
+    soup = soup_html(app, "with-revealjs-slide/has-theme.html")
+    css_hrefs = [elm["href"] for elm in soup.find_all("link", rel="stylesheet")]
+    assert "../_static/custom.css" in css_hrefs
+
+
+@pytest.mark.sphinx("revealjs", testroot="default")
+def test_config(app: SphinxTestApp, status, warning):  # noqa
+    soup = soup_html(app, "with-revealjs-slide/has-conf.html")
+    assert 'Object.assign(revealjsConfig, {"transition": "none"});' in str(
+        soup.find_all("script")[-1]
+    )
+
+
+@pytest.mark.sphinx("revealjs", testroot="default")
+def test_config_as_content(app: SphinxTestApp, status, warning):  # noqa
+    soup = soup_html(app, "with-revealjs-slide/has-content.html")
+    assert "Object.assign(revealjsConfig, {\n" in str(soup.find_all("script")[-1])
+    assert '"transition": "none"\n' in str(soup.find_all("script")[-1])
+    assert "});" in str(soup.find_all("script")[-1])
