4891c6f0482453cf0671cf74f0b6b7906eae16be
Kazuya Takei <myself@attakei.net>
2022-07-31 23:18:32 +0900

    chore: Catch DeprecationWarning of sphinx in running pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,24 @@
 """Configuration for pytest."""
+import inspect
+
 import pytest
+from _pytest.config import Config
+from sphinx import deprecation
 from sphinx.testing.path import path
 
 pytest_plugins = "sphinx.testing.fixtures"
-
 collect_ignore = ["roots"]
+
+
+def pytest_configure(config: Config):  # noqa
+    for name, klass in inspect.getmembers(deprecation, inspect.isclass):
+        if not issubclass(klass, DeprecationWarning) and not issubclass(
+            klass, PendingDeprecationWarning
+        ):
+            continue
+        config.addinivalue_line(
+            "filterwarnings", f"error::{klass.__module__}.{klass.__name__}"
+        )
 
 
 @pytest.fixture(scope="session")
