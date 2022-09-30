d1aa659da436278b9933ca9407d12879966c21ed
Kazuya Takei <myself@attakei.net>
2022-09-30 12:42:13 +0900

    fix: Rename highlighting module
    - For collect components of syntax-highlighting
    - It does not change behavior
    
    Refs: #126

--- sphinx_revealjs/__init__.py
+++ sphinx_revealjs/__init__.py
@@ -125,7 +125,7 @@ def setup(app: Sphinx):
     app.add_config_value("revealjs_script_conf", None, True)
     app.add_config_value("revealjs_script_plugins", [], True)
     app.add_html_theme("sphinx_revealjs", str(get_theme_path("sphinx_revealjs")))
-    app.setup_extension("sphinx_revealjs._ext.code_block")
+    app.setup_extension("sphinx_revealjs._ext.highlightings")
     app.setup_extension("sphinx_revealjs._ext.notes")
     return {
         "version": __version__,
