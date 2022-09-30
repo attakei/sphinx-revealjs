a05a15f78a99bf0a29f39a1cdc2474406886f50b
Kazuya Takei <myself@attakei.net>
2022-09-30 13:16:29 +0900

    feat: Add `revealjs-literalinclude` directive
    - This is extends from `literalinclude` for step-by-step highlighting
    
    Refs: #126, https://revealjs.com/code/

--- sphinx_revealjs/_ext/highlightings.py
+++ sphinx_revealjs/_ext/highlightings.py
@@ -1,7 +1,7 @@
 """Internal extension for highlighting of Reveal.js."""
 from docutils.parsers.rst import directives
 from sphinx.application import Sphinx
-from sphinx.directives.code import CodeBlock
+from sphinx.directives.code import CodeBlock, LiteralInclude
 
 from .. import __version__
 
@@ -22,8 +22,31 @@ class RevealjsCodeBlock(CodeBlock):  # noqa: D101
         return nodes
 
 
+class RevealjsLiteralInclude(LiteralInclude):
+    """Extends from ``literalinclude`` directive.
+
+    This has ``data-line-numbers`` for step-by-step code highlighting.
+    See `it <https://revealjs.com/code/>`_ for more information.
+    """
+
+    option_spec = {
+        **LiteralInclude.option_spec,
+        "data-id": directives.unchanged,
+        "data-line-numbers": directives.unchanged,
+    }
+
+    def run(self):  # noqa: D102
+        nodes = super().run()
+        if self.options.get("data-line-numbers"):
+            nodes[0]["data-line-numbers"] = self.options.get("data-line-numbers")
+        if self.options.get("data-id"):
+            nodes[0]["data-id"] = self.options.get("data-id")
+        return nodes
+
+
 def setup(app: Sphinx):  # noqa: D103
     app.add_directive("revealjs-code-block", RevealjsCodeBlock)
+    app.add_directive("revealjs-literalinclude", RevealjsLiteralInclude)
     return {
         "version": __version__,
         "env_version": 2,
