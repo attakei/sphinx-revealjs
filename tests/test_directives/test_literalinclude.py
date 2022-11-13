1abfab6e034bdfdee0fa2a807170ee77b697121b
Kazuya Takei <myself@attakei.net>
2022-11-13 17:55:31 +0900

    feat: Enable 'data-ln-start-from' option for code blocks
    - Add into revealjs-code-block and revealjs-literalinclude.
    - Generate it as html attributes by writer.
    - Checked that is passed by pytest.
    
    Refs: #134

--- tests/test_directives/test_literalinclude.py
+++ tests/test_directives/test_literalinclude.py
@@ -17,3 +17,11 @@ def test_dataline(app: SphinxTestApp, status, warning):  # noqa
     code_tag = soup.find_all("code")[1]
     assert "python" in code_tag.attrs["class"]
     assert "data-line-numbers" in code_tag.attrs
+
+
+@pytest.mark.sphinx("revealjs", testroot="default")
+def test_dataline_start_from(app: SphinxTestApp, status, warning):  # noqa
+    soup = soup_html(app, "with-revealjs-literalinclude.html")
+    code_tag = soup.find_all("code")[2]
+    assert "data-line-numbers" in code_tag.attrs
+    assert code_tag.attrs["data-ln-start-from"] == "5"
