276aaadb286964cf37c4a9a5e14097ced9b2ef09
Kazuya Takei <myself@attakei.net>
2022-12-06 00:13:05 +0900

    fix(screenshot): Rename to fix missing name

--- tests/test_extensions/test_screenshot.py
+++ tests/test_extensions/test_screenshot.py
@@ -10,7 +10,7 @@ from testutils import soup_html
     testroot="default",
     confoverrides={"extensions": ["sphinx_revealjs", "sphinx_revealjs.ext.screenshot"]},
 )
-def test_not_generate_screenshot(app: SphinxTestApp, status, warning):  # noqa
+def test_generate_screenshot(app: SphinxTestApp, status, warning):  # noqa
     app.build()
     image_path = app.outdir / "_images/ogp/index.png"
     assert image_path.exists()
