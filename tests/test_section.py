a6d7d961e0a5346d220b6055fea9c919931e8761
Kazuya Takei <myself@attakei.net>
2024-02-04 14:36:03 +0900

    ci: Add test for behavior about level-4 section syntax

--- tests/test_section.py
+++ tests/test_section.py
@@ -23,3 +23,12 @@ def test_count_vertical_sections(app: SphinxTestApp, status, warning):  # noqa
     main = soup.find("div", {"role": "main"})
     section = main.find_all("section", recursive=False)[1]
     assert len(section.find_all("section", recursive=False)) == 3
+
+
+@pytest.mark.sphinx("revealjs", testroot="default")
+def test_lv4_section(app, status, warning):  # noqa
+    soup = soup_html(app, "index.html")
+    h4 = soup.h4
+    assert h4 is not None
+    # Lv4 content is sibling pf Lv3 content (not splitted as other slides)
+    assert h4.parent.h3 is not None
