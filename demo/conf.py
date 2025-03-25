1e292c60d21e3c49be208a4db348cb0b090d90c4
Kazuya Takei <myself@attakei.net>
2025-03-26 00:04:34 +0900

    demo(refactor): To work build with Reveal.js 5.2.0
    Refs: #205

--- demo/conf.py
+++ demo/conf.py
@@ -20,11 +20,11 @@ extensions = [
     "sphinx.ext.todo",
     # My extensions
     "atsphinx.mini18n",
+    "atsphinx.toybox.sass",
     "oembedpy.adapters.sphinx",
     "rst_package_refs.sphinx",
     "sphinxcontrib.budoux",
     "sphinxcontrib.gtagjs",
-    "sphinxcontrib.sass",
     # Third-party extensions
     "sphinxext.opengraph",
     # This project
@@ -99,13 +99,6 @@ if "GTAGJS_IDS" in os.environ:
     gtagjs_ids = os.environ["GTAGJS_IDS"].split(",")
 # - sphinxcontrib.budoux
 budoux_targets = ["h1", "h2", "h3"]
-# - sphinxcontrib.sass
-sass_src_dir = "_sass"
-sass_out_dir = "_static"
-sass_targets = {"custom.scss": "custom.css"}
-sass_include_paths = [
-    get_revealjs_path() / "css" / "theme",
-]
 # - sphinxext.opengraph
 ogp_site_url = os.environ.get("DEMO_URL_BASE", "http://localhost:8000/")
 ogp_custom_meta_tags = [
@@ -116,6 +109,10 @@ ogp_custom_meta_tags = [
 mini18n_default_language = "en"
 mini18n_support_languages = ["en", "ja"]
 mini18n_basepath = "/sphinx-revealjs/"
+# - atsphinx.toybox.sass
+sass_load_paths = [
+    get_revealjs_path() / "css" / "theme",
+]
 
 
 def update_ogp(app, config):
