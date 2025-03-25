f26c64e97ed057331c485c882e57b1257f6e857a
Kazuya Takei <myself@attakei.net>
2025-03-26 01:08:05 +0900

    feat(sass): Define new sub-extension (empty)
    - This is to compile sass and scss by dart-sass.
    - This is alternative to sphinxcontrib-sass
    
    Refs: #205

---
+++ sphinx_revealjs/ext/sass/__init__.py
@@ -0,0 +1,12 @@
+"""SASS compile bridge."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+
+def setup(app: Sphinx):
+    pass
