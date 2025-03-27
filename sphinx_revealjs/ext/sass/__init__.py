b5d20d569e474b6f370878b57d4363b504fd0dcb
Kazuya Takei <myself@attakei.net>
2025-03-27 22:29:30 +0900

    feat(sass): Auto target finder

--- sphinx_revealjs/ext/sass/__init__.py
+++ sphinx_revealjs/ext/sass/__init__.py
@@ -45,11 +45,19 @@ def build_sass_sources(app: Sphinx, env: BuildEnvironment):
     logger.debug("Building stylesheet files")
     src_dir = configure_path(app.confdir, app.config.sass_src_dir)
     out_dir = configure_path(app.confdir, app.config.sass_out_dir)
-    targets: Targets = app.config.sass_targets
 
     options = [f"--style={app.config.sass_output_style}"]
     if app.config.sass_include_paths:
         options += [f"--load-path={p}" for p in app.config.sass_include_paths]
+
+    targets: Targets = app.config.sass_targets
+    if app.config.sass_auto_targets:
+        for s in src_dir.glob("*.s[ac]ss"):
+            if s.name.startswith("_"):
+                continue
+            rel_src = s.relative_to(src_dir)
+            rel_dst = rel_src.parent / f"{rel_src.stem}.css"
+            targets[rel_src] = rel_dst
 
     for src, dst in targets.items():
         src_ = src_dir / src
@@ -64,4 +72,5 @@ def setup(app: Sphinx):
     app.add_config_value("sass_out_dir", None, "html")
     app.add_config_value("sass_targets", {}, "html")
     app.add_config_value("sass_output_style", "expanded", "html")
+    app.add_config_value("sass_auto_targets", False, "html")
     app.connect("env-updated", build_sass_sources)
